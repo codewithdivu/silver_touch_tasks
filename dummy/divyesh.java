de7cebc280bf595e46113044b2f93d631a9832ee
= <=>
2024-01-30 09:27:25 +0530

    [fixed] : folder structure of website

--- dummy/divyesh.java
+++ dummy/divyesh.java
@@ -1,6 +1,11 @@
+import java.util.*;
+
 public class divyesh {
 
     public static void main(String[] args) {
-        System.out.println("hello divyesh bhai");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hey dear kindly enter your name");
+        String name = sc.nextLine();
+        System.out.println("Your name is " + name);
     }
 }
